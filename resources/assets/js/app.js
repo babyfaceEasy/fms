1fc91d97e17420772328a736a7214f50d28498df
IC-SOL1\imatthew <ighotouch@gmail.com>
2016-09-15 15:25:04 +0100

    setting up gulp

---
+++ resources/assets/js/app.js
@@ -0,0 +1,12 @@
+var Vue = require('vue');
+import Greeter from './components/Greeter.vue';
+
+new Vue({
+    el: '#app',
+
+
+
+    ready() {
+        alert('Vue and Vueify all set to go!');
+    }
+});
---
+++ resources/assets/js/components/Greeter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello, </h1>
+
+        <input type="text" v-model="name">
+    </div>
+</template>
+
+
+
+<style>
+    h1 {
+        color: red;
+    }
+</style>
